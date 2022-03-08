9c90dcf9a08b771f6c30c04d05ce22720499e4c4
SimonePavone <simonebest56@gmail.com>
2022-03-08 16:31:52 +0100

    Push davide

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,8 +4,10 @@ document.getElementById("contenitore_target").innerHTML = nomeUtente;
 
 const cognomeUtente = prompt("Inserisci il tuo cognome");
 console.log("cognomeUtente");
-document.getElementById("contenitore_target2").innerHTML = cognomeUtente;
+document.getElementById("contenitore_target").innerHTML = cognomeUtente;
 
 const coloreUtente = prompt = prompt("Inserisci il tuo colore preferito");
 console.log("coloreUtente");
-document.getElementById("contenitore_target3").innerHTML = coloreUtente;
+document.getElementById("contenitore_target").innerHTML = coloreUtente;
+
+console.log(`la tua password è ${nomeUtente} ${cognomeUtente} ${coloreUtente}`)
